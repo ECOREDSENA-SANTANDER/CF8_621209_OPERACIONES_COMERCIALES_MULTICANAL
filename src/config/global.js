c0303a4754a51c122393d28a7652222b618aa99c
IngLeoCastellanos <ingleonardocastellanos@gmail.com>
2026-07-27 08:59:16 -0500

    AVANCE

--- src/config/global.js
+++ src/config/global.js
@@ -191,7 +191,7 @@ export default {
         {
           nombre: 'Claudia Johanna Gómez Pérez',
           cargo:
-            'Profesional 06. Responsable Ecosistema de Recursos Educativos Digitales (RED)',
+            'Profesional G06. Responsable Ecosistema de Recursos Educativos Digitales (RED)',
           centro: 'Centro Agroturístico - Regional Santander',
         },
         {
